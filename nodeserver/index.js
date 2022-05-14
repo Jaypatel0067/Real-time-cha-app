3d11625aba987bbec8c418f30f09a446b453ab03
Jaypatel0067 <99090747+Jaypatel0067@users.noreply.github.com>
2022-05-14 19:39:57 -0400

    Add files via upload

---
+++ nodeserver/index.js
@@ -0,0 +1,26 @@
+// node server for handling soket io
+var mysql = require('mysql');
+
+
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: '*',
+    }
+  });
+const user = {};
+
+io.on('connection', socket=>{
+    socket.on('new-user-joined', name=>{
+         console.log(name);
+        user[socket.id]=name;
+        socket.broadcast.emit('user-joined', name)
+    });
+
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive', {message:message, name:  user[socket.id]})
+    });
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', user[socket.id]);
+        delete user[socket.id]
+    });
+})
